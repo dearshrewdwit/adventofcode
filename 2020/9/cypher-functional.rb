50ddde18948cc27a65b7a848b1b9d6e6c9ac5a18
Edward Withers <dearshrewdwit@gmail.com>
2020-12-09 13:48:03 +0000

    functional implementation

---
+++ 2020/9/cypher-functional.rb
@@ -0,0 +1,33 @@
+def weak_number(numbers, preamble = 25)
+  numbers.filter_map.with_index do |number, index|
+    next if index < preamble
+    number if numbers[(index-preamble)..(index-1)].combination(2).map(&:sum).none? {|sum| sum == number }
+  end.first
+end
+
+def encryption_weakness(numbers)
+  min_add_max(set(numbers))
+end
+
+def numbers
+  File.open('input.txt', 'r') { |file| file.readlines(chomp: true).map(&:to_i) }
+end
+
+def set(numbers)
+  weak_number = weak_number(numbers)
+  numbers.filter_map.with_index { |number, index| find(numbers[index..-1], 0, weak_number) }.first
+end
+
+def find(numbers, index, sum, set = [])
+  return set if set.length >= 2 && set.sum == sum
+  return if index == numbers.length
+  return if set.sum > sum
+  find(numbers, index + 1, sum, set + [numbers[index]])
+end
+
+def min_add_max(set)
+  set.min + set.max
+end
+
+p weak_number(numbers)
+p encryption_weakness(numbers)
