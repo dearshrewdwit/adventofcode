60c6c0d553d8a4b371479a397f37bb4ee338106f
dearshrewdwit <dearshrewdwit@gmail.com>
2022-12-02 12:29:12 +0000

    remove mapping

--- 2022/day2/part2.ts
+++ 2022/day2/part2.ts
@@ -1,21 +1,15 @@
 const fs = require('fs/promises')
 
 const rules = {
-  'rock': 'scissors',
-  'paper': 'rock',
-  'scissors': 'paper'
-}
-
-const dict = {
-  'rock': ['A'],
-  'paper': ['B'],
-  'scissors': ['C']
+  'A': 'C',
+  'B': 'A',
+  'C': 'B'
 }
 
 const movePts = {
-  'rock': 1,
-  'paper': 2,
-  'scissors': 3
+  'A': 1,
+  'B': 2,
+  'C': 3
 }
 
 const resultPts = {
@@ -30,16 +24,13 @@ const movesForResult = {
   'Z': (oppMove: string) => Object.keys(rules).find((move) => rules[move as keyof typeof rules] === oppMove) ?? ""
 }
 
-const decryptMove = (moveEnc: string) => Object.keys(dict).find((move) => dict[move as keyof typeof dict].includes(moveEnc)) ?? ""
-
 export const main = async () => {
   const data: string = await fs.readFile('./day2/data.txt', 'utf8')
 
   return data.trim().split("\n").reduce((acc: number, val: string) => {
-    const [oppMoveEnc, resultEnc] = val.split(" ")
-    const oppMove: string = decryptMove(oppMoveEnc)
-    const myMove: string = movesForResult[resultEnc as keyof typeof movesForResult](oppMove)
-    const roundPts = movePts[myMove as keyof typeof movePts] + resultPts[resultEnc as keyof typeof resultPts]
+    const [oppMove, result] = val.split(" ")
+    const myMove: string = movesForResult[result as keyof typeof movesForResult](oppMove)
+    const roundPts = movePts[myMove as keyof typeof movePts] + resultPts[result as keyof typeof resultPts]
 
     return acc + roundPts
   }, 0)
