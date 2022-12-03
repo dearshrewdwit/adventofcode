dbac572f0b4f273f5bc2f9ab21eaba5307e0d97b
dearshrewdwit <dearshrewdwit@gmail.com>
2022-12-03 14:07:13 +0000

    tidy

--- 2022/day3/src/part1.ts
+++ 2022/day3/src/part1.ts
@@ -3,12 +3,15 @@ const fs = require('fs/promises')
 export const main = async () => {
   const data: string = await fs.readFile('./day3/input.txt', 'utf8')
 
-  return data.trim().split("\n").reduce((acc, rucksack) => {
-    const compartment1 = rucksack.slice(0, rucksack.length/2)
-    const compartment2 = rucksack.slice(rucksack.length/2)
-    const sharedItem = compartment1.split('').filter(item => compartment2.includes(item))[0]
-    const offset = (/[a-z]/).test(sharedItem) ? 96 : 38
+  return data
+          .trim()
+          .split("\n")
+          .reduce((acc, rucksack) => {
+            const compartment1 = rucksack.slice(0, rucksack.length/2)
+            const compartment2 = rucksack.slice(rucksack.length/2)
+            const sharedItem = compartment1.split('').filter(item => compartment2.includes(item))[0]
+            const offset = (/[a-z]/).test(sharedItem) ? 96 : 38
 
-    return acc + sharedItem.charCodeAt(0) - offset
-  }, 0)
+            return acc + sharedItem.charCodeAt(0) - offset
+          }, 0)
 }
